925d36eb6ca7cd9d96030e1c4fa013b668bb3414
kaltath-darro <lookasz.m.r@poczta.fm>
2019-08-26 17:45:24 +0200

    pętla keep alive

--- Src/HuntingClient/HuntingClient.cs
+++ Src/HuntingClient/HuntingClient.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Threading.Tasks;
+using Gravityzero.Console.Utility.Tools;
 using GravityZero.HuntingClient.Infrastructure;
 using GravityZero.HuntingClient.Infrastructure.Domain;
 using GravityZero.HuntingClient.Infrastructure.Result;
@@ -7,6 +9,7 @@ namespace GravityZero.HuntingClient
 {
     public class Client : IHuntingClient 
     {
+        private readonly string path = "http://projecthunter.pl:80/Api/User/KeepAlive";
         private IAuthentication authentication;
         private IUserManipulation userManipulation;
         private IStateChanger changer;
@@ -15,6 +18,27 @@ namespace GravityZero.HuntingClient
         public Client()
         {
             this.authentication = new AuthenticationDisconnect();
+            Task.Run(async () => {
+                short connectionErrorCounter = 0;
+                while(true)
+                {
+                    ConnectorResult<Guid>  getResult = await WinApiConnector.RequestGet<Guid>(path);
+                    if (getResult.IsSuccess)
+                    {
+                        connectionErrorCounter = 0;
+                    }
+                    else
+                    {
+                        connectionErrorCounter += 1;
+                        if (connectionErrorCounter > 10)
+                        {
+                            //co zrobić na błąd połączebnia
+                            //stworzyć obiekt odpowiadający na zbieranie danych do czasu odzyskania połączenia???
+                        }
+                    }
+                    await Task.Delay(2000);
+                }
+            });
         }
         
         public HuntingClientResult Login(Authentication authentication)
